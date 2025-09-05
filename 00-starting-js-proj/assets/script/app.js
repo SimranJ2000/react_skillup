c3c465ee6236f60d96c6b2ff60a906660f9316bf
some_name <simran.jahan@capgemini.com>
2025-09-05 23:33:23 +0530

    Use of function in Js

---
+++ 00-starting-js-proj/assets/script/app.js
@@ -0,0 +1,15 @@
+//import { apikey } from"./util.js";
+//import apikey from"./util.js";
+//import{ apikey, abc } from"./util.js";
+//import * as content from"./util.js";
+//let userName="Mishika";
+//console.log(userName);
+//console.log(userName);
+//console.log(`hello ${userName}, Welcome to the world of JavaScript`);
+function greet(userName , message ){
+    console.log("hello jahan");
+    console.log(userName);
+     console.log(message);
+}
+greet("simran", "good luck for future");
+greet("Akash" ,"all the very best");
---
+++ 00-starting-js-proj/assets/script/util.js
@@ -0,0 +1,4 @@
+//export let apikey="your-api-key-here";
+//export default"your-api-key here hai";
+//export let apikey ="yours-api-key-here";
+//export let abc ="hello world";
