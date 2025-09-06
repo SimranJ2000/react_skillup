2b012bf7d42ce445783b9260dcd5cc279e4b276c
some_name <simran.jahan@capgemini.com>
2025-09-06 20:39:13 +0530

    working with function

---
+++ 00-starting-js-proj/assets/script/func.js
@@ -0,0 +1,15 @@
+/*Exercise: Working with Functions
+Your task is to write a new function that should be named combine and have the following characteristics:
+
+Accept three input values
+
+Calculate a new value based on the three input values: a * b / c (if a, b & c are the input values)
+
+Return the calculated result*/
+function combine(a,b,c){
+    return a*b/c;
+}
+    const result= combine(10,2,5);
+    console.log(result);
+//output- 4
+    
